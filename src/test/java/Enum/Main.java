ffe8555a2d5433a0280c2ab2d35628746575778a
Користувач <dimabekar8@gmail.com>
2022-02-23 18:34:48 +0200

    Homework for homework with exception

--- src/test/java/Enum/Main.java
+++ src/test/java/Enum/Main.java
@@ -1,14 +1,22 @@
+/*
+ * Copyright (c) 1994, 2022, My company and/or its affiliates. All rights reserved.
+ * My company PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
 package Enum;
 
 import java.util.Scanner;
-
+/**This class created for did homework
+ */
 public class Main {
     public static void main(String[] args) {
-//        Month month=Month.DECEMBER;
         Month month1 = Month.JANUARY;
-//        month1.nomer1();
-        System.out.println(Month.JANUARY.name());
 
+
+
+        //course for homework
+        //this course do Вивести всі місяці які мають таку саму кількість днів.
+        //Вивести на екран всі місяці які мають меншу кількість днів.
+        //Вивести на екран всі місяці які мають більшу кількість днів.
         // 3 4 5
 //        for (Month month  : Month.values()) {
 //          if (month.day==31){
@@ -26,6 +34,10 @@ public class Main {
 //
 //        }
 //
+
+        //course for homework
+        //this course do Вивести на екран всі місяці які мають парну кількість днів.
+        //Вивести на екран всі місяці які мають непарну кількість днів.
         // 8 9
 //        for (Month month : Month.values()) {
 //            if (month.day%2==0){
@@ -34,6 +46,7 @@ public class Main {
 //                System.out.println("Month arent have couple "+month);
 //            }
 //        }
+
 
         //6
 //        Season season=Season.SUMMER;
@@ -50,6 +63,8 @@ public class Main {
 //        }
 
 
+        //i do here Перевірити чи є такий місяць (місяць вводимо з консолі, передбачити, щоб регістр
+        //букв був не важливим ).
 //        //1
 //        Scanner scanner = new Scanner(System.in);
 //        System.out.print("Enter the month: ");
@@ -64,6 +79,9 @@ public class Main {
 //
 //
 //        }
+        //this code do
+        // Вивести на екран чи введений з консолі місяць має парну кількість днів.
+        //
         //10
 //        Scanner scanner=new Scanner(System.in);
 //        System.out.println("Введіть місяць:");
--- src/test/java/Enum/Season.java
+++ src/test/java/Enum/Season.java
@@ -1,5 +1,10 @@
+/*
+        * Copyright (c) 1994, 2022, My company and/or its affiliates. All rights reserved.
+        * My company PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+        */
 package Enum;
-
+/**This class created for did homework
+ */
 public enum Season {
     WINTER,SUMMER,SPRING,AUTUMN;
 
